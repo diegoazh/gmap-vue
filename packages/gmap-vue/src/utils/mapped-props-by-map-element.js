a1639b2f92d6fea7e1519901b397141e8c81b160
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2021-06-14 02:17:29 -0300

    feat(all): update marker component and its documentation

--- packages/gmap-vue/src/utils/mapped-props-by-map-element.js
+++ packages/gmap-vue/src/utils/mapped-props-by-map-element.js
@@ -453,6 +453,64 @@ export const mapMappedProps = {
   },
 };
 
+export const markerMappedProps = {
+  animation: {
+    twoWay: true,
+    type: Number,
+  },
+  attribution: {
+    type: Object,
+  },
+  clickable: {
+    type: Boolean,
+    twoWay: true,
+    default: true,
+  },
+  cursor: {
+    type: String,
+    twoWay: true,
+  },
+  draggable: {
+    type: Boolean,
+    twoWay: true,
+    default: false,
+  },
+  icon: {
+    twoWay: true,
+  },
+  label: {},
+  opacity: {
+    type: Number,
+    default: 1,
+  },
+  options: {
+    type: Object,
+  },
+  place: {
+    type: Object,
+  },
+  position: {
+    type: Object,
+    twoWay: true,
+  },
+  shape: {
+    type: Object,
+    twoWay: true,
+  },
+  title: {
+    type: String,
+    twoWay: true,
+  },
+  zIndex: {
+    type: Number,
+    twoWay: true,
+  },
+  visible: {
+    twoWay: true,
+    default: true,
+  },
+};
+
 export default {
   autocompleteMappedProps,
   circleMappedProps,
