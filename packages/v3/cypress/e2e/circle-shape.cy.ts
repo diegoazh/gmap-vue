819c8026d079a2c52486e47a70ca6ae64c1cbd13
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2023-05-30 11:27:58 -0300

    test(v3): update e2e test for info-window and add tests for circle, rectangle and polyline

---
+++ packages/v3/cypress/e2e/circle-shape.cy.ts
@@ -0,0 +1,10 @@
+describe('CircleShape component', () => {
+  it('should contain a circle shape with 5 points', function () {
+    cy.visit('/');
+    cy.get('button[name=circle]').click();
+    cy.get('[aria-label=Map]').should('be.visible');
+    cy.get(
+      '[aria-label=Map] > div > div > [style="position: absolute; left: 0px; top: 0px; z-index: 106; width: 100%;"] > div > div'
+    ).should('have.length', 5);
+  });
+});
--- packages/v3/cypress/e2e/info-window.cy.ts
+++ packages/v3/cypress/e2e/info-window.cy.ts
@@ -1,5 +1,5 @@
 describe('InfoWindow component', () => {
-  it('should ', function () {
+  it('should click on the 3 clickable markers and display a different text on the info-window when each of it is clicked', function () {
     cy.visit('/');
     cy.get('button[name=info-window]').click();
     cy.get('div[role=button]').each((el, index) => {
